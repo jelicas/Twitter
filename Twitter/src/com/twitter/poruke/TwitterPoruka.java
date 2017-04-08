a5a9c754ddfcdb6dff108b91515807413cd95788
jelicastanojevicc <jelicastanojevicc@gmail.com>
2017-04-08 19:12:52 +0200

    Napisana javadoc dokumentacija za klasu TwitterPoruka

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,31 +1,78 @@
 package com.twitter.poruke;
 
+/**
+ * Klasa koja predstavlja twitter poruku odredjenog korisnika
+ * @author Jelica Stanojevic
+ * @version 1.0
+ * 
+ */
 public class TwitterPoruka {
 
+	/**
+	 * Ime korisnika
+	 */
 	private String korisnik;
+	
+	/**
+	 * Tekst poruke
+	 */
 	private String poruka;
 
+	
+	/**
+	 * Vraca vrednost atributa korisnik 
+	 * @return korisnik , njegovo ime kao String
+	 */
 	public String getKorisnik() {
 		return korisnik;
 	}
 
+	/**
+	 * Postavlja novu vrednost za atribut korisnik
+	 * @param korisnik , novo ime za korisnika
+	 * @throws java.lang.RuntimeException ako je uneo ime:
+	 * 
+	 * <ul>
+	 * 		<li>null</li>
+	 * 		<li>prazan String</li>
+	 * </ul>
+	 */
 	public void setKorisnik(String korisnik) {
 		if (korisnik == null || !korisnik.isEmpty())
 			throw new RuntimeException("Ime korisnika mora biti uneto");
 		this.korisnik = korisnik;
 	}
 
+	/**
+	 * Vraca vrednost atributa poruka
+	 * @return poruka , tekst poruke kao String
+	 */
 	public String getPoruka() {
 		return "poruka";
 	}
 
+	/**
+	 * Postavlja novu vrednost za atribut poruka
+	 * @param poruka , novi text poruke
+	 * @throws java.lang.RuntimeException ako je uneo poruku:
+	 * 
+	 * <ul>
+	 * 		<li>null</li>
+	 * 		<li>prazan String</li>
+	 * 		<li>String duzi od 140 karaktera</li>
+	 * </ul>
+	 */
 	public void setPoruka(String poruka) {
 		if (this.poruka == null || this.poruka == new String("") || this.poruka.length() > 140)
 			throw new RuntimeException("Poruka mora biti uneta i mora imati najvise 140 znakova");
 		this.poruka = poruka;
 	}
 
+	/**
+	 * Vraca String sa svim podacima twitter poruke
+	 * @return Ime korisnika i poruku sa odgovarajucom porukom.
+	 */
 	public String toString() {
 		return "KORISNIK:" + korisnik + " PORUKA:" + poruka;
 	}
-} 
+}
